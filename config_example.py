16da58775e2ed1d6c90a9a8eff41338524e1fc85
franck <f.roland@dental-monitoring.com>
2016-07-27 23:15:43 +0200

    Update config_example.py

--- config_example.py
+++ config_example.py
@@ -2,3 +2,7 @@ PRIVATE_TOKEN = "Your private token you can find on Gitlab"
 HOST = 'https://gitlab.h42.fr'
 PROJECTS = ['list of projects names']
 MANAGEMENT_ENV = 'kanban'
+CACHE_CONFIG = {
+    "type": "redis",
+    "host": "127.0.0.1"
+}
